d0bbda0187a51473526a2fd927f2a144080102cb
gms4 <gms4@cin.ufpe.br>
2022-08-25 17:10:05 -0300

    task1 done

---
+++ Task1RPNcalc.java
@@ -0,0 +1,52 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Task1RPNcalc {
+
+    public static void main (String[] args) {
+
+        File file = new File("Calc1.stk");
+        ArrayList<String> input = new ArrayList<String>();
+
+        try (Scanner in = new Scanner(file, StandardCharsets.UTF_8)) {
+            while(in.hasNextLine()) {
+                String aux = in.nextLine();
+                input.add(aux);
+            }
+        } catch (IOException e ){
+            e.printStackTrace();
+        }
+
+        Iterator<String> iterator = input.iterator();
+        Stack<Integer> stackpile = new Stack<Integer>();
+
+        Integer output = 0;
+
+        while(iterator.hasNext()) {
+            String rpnElemenString = iterator.next();
+
+            if (stackpile.size() < 2) {
+                output = Integer.parseInt(rpnElemenString);
+            } else {
+                Integer secondOp = stackpile.pop();
+                Integer firstOp = stackpile.pop();
+
+                if (rpnElemenString.equals("+")) output = firstOp + secondOp;
+                else if (rpnElemenString.equals("-")) output = firstOp - secondOp;
+                else if (rpnElemenString.equals("*")) output = firstOp * secondOp;
+                else if (rpnElemenString.equals("/")) output = firstOp / secondOp;
+                 
+            }
+
+            stackpile.add(output);
+        }
+
+        Integer out = stackpile.pop();
+        System.out.println(out);
+    }
+}
