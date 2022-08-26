e67fa89eba0c801508cb23ee98541d8f4b163095
gms4 <gms4@cin.ufpe.br>
2022-08-26 20:17:40 -0300

    fazendo de outra forma

--- Task1RPNcalc2.java
+++ Task1RPNcalc2.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Stack;  
-import java.lang.Math;
 
 public class Task1RPNcalc2 {
     public static void main(String[] args) throws IOException{
